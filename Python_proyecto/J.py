264f7004394da8c7676dbbd55bc25bb0bd7f7e86
Juan-771 <abrilromanj@gmail.com>
2025-02-11 13:15:20 -0500

    tercer commit

--- Python_proyecto/J.py
+++ Python_proyecto/J.py
@@ -1,5 +1,5 @@
 import json
-from funciones import *
+import funciones_a
 # Función para abrir el archivo JSON
 def abrirJSON():
     dicFinal={}
@@ -10,16 +10,6 @@ def abrirJSON():
 def guardarJSON(dic):
     with open(f"./campers.json",'w') as outFile:
         json.dump(dic,outFile)
-
-'''def abrirJSON():
-    dicFinal={}
-    with open(f"./trainers.json",'r') as openFile:
-        dicFinal=json.load(openFile)
-    return dicFinal
-
-def guardarJSON(dic):
-    with open(f"./trainers.json",'w') as outFile:
-        json.dump(dic,outFile)'''
 
 
 
@@ -50,4 +40,29 @@ if opt == "1":
         
         if camper_encontrado:
             # Si el NumId se encuentra, iniciar sesión
-            print(f" Bienvenido de nuevo, {camper_encontrado['Nombres']}!")
+            print(f" Bienvenido de nuevo, {camper_encontrado['nombre']}!")
+            print(funciones_a.menucamper())
+        
+
+if opt == "3":
+    print("Bienvenido coordinador/a")
+    id=print(input("Digita tu Id "))
+    
+    print ("¿Que deseas hacer?")
+    print ("1. Ver grupos")
+    print ("2. Asignar")
+    opc = input (":")
+    print(funciones_a.menucoordinador())
+    
+if opt == "2":
+        print ("Bienvenido Trainer ")
+        print ("Digita tu Id")
+
+        print (funciones_a.menutrainer())
+
+        
+        
+
+
+
+    
---
+++ Python_proyecto/funciones_a.py
@@ -0,0 +1,20 @@
+def menucamper():
+    print("1. Para ver su informacion")
+    print ("2. Para ver su estado (Inscrito o aprobado)")
+
+def menucoordinador():
+    print ("1. Camper aprobados")
+    print ("2. Camper encargados ")
+    print ("3. Ruta de entrenamiento")
+    print ("4.Fecha") 
+    print ("5. Salones")
+
+
+def menutrainer ():
+    print ("1.Ver notas ")
+    print ("2.Asignar notas ")
+    print ("3.ver campers ")
+    print ("4. ver salones ")
+    print ("5. Salir ")
+
+
